6a2f1782e7588e7c3d3990050611456f1fa6dd7f
Daniel Williams <Ishamel44@gmail.com>
2023-02-18 11:02:26 -0500

    Skill Check One

---
+++ Skill_Check_1.py
@@ -0,0 +1,10 @@
+Grocery_List = ["Eggs", "Milk", "Bread", "Paper Towels"]
+Food_Goes = {
+    "Fridge": "Cold Items",
+    "Cabinet": "Room Temp Items"
+}
+food_tuple = ("Cereal," "Jello", "Chips", "Juice")
+print("Hello World!")
+print(Food_Goes["Cabinet"])
+print(Grocery_List[2])
+print(food_tuple[1])
